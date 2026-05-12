b362a0763f9d981d9ecc4d9cad80d3f323480e48
fcharles <f.18charles@gmail.com>
2026-05-12 11:31:26 +0300

    fix(export): added date filter to the export function from the transaction repo

--- backend/internal/repository/transaction_repo.go
+++ backend/internal/repository/transaction_repo.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"errors"
+	"time"
 
 	"github.com/f18charles/piggy-bank/backend/internal/models"
 	"github.com/f18charles/piggy-bank/backend/internal/utils"
@@ -44,6 +45,16 @@ func (tr *TransactionRepo) UpdateTransaction(tx *models.Transaction) error {
 func (tr *TransactionRepo) ListTransactionsByUser(userID uuid.UUID) ([]models.Transaction, error) {
 	txs := []models.Transaction{}
 	result := tr.Db.Where("user_id = ?", userID).Preload("Account").Preload("Category").Find(&txs)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return txs, nil
+}
+
+func (tr *TransactionRepo) ListTransactionsByUserSince(user_id uuid.UUID, since_date time.Time) ([]models.Transaction, error) {
+	var txs []models.Transaction
+	result := tr.Db.Where("user_id = ? AND COALESCE(transaction_date, created_at) >= ?", user_id, since_date).Preload("Account").Preload("Category").Order("transaction_date DESC").Find(&txs)
+
 	if result.Error != nil {
 		return nil, result.Error
 	}
