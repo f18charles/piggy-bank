10414fe786cd43da1eb09967ff78285d618ee679
fcharles <f.18charles@gmail.com>
2026-05-12 10:12:50 +0300

    fix(models) removed default from the primary keys of account and transction models

--- backend/internal/models/account.go
+++ backend/internal/models/account.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Account struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:primaryKey" json:"id"`
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
 	UserID    uuid.UUID `gorm:"type:uuid;not null" json:"user_id"`
 	Name      string    `gorm:"not null" json:"name"`
 	Type      string    `gorm:"not null" json:"type"`
