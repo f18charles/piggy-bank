731ec4b8d1c812083403a23b9aa5d21a4d2f239f
f18charles <f.18charles@gmail.com>
2026-05-25 12:40:39 +0300

    feat(logger): implement logger initialization based on environment

---
+++ backend/internal/utils/logger.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"log/slog"
+	"os"
+)
+
+func InitLogger(env string) {
+	var level slog.Level
+	if env == "production" {
+		level = slog.LevelWarn
+	} else {
+		level = slog.LevelDebug
+	}
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOption{
+		Level:level
+	})))
+}
