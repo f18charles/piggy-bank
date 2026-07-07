d0e8ff5f4521fb6c87e9e7c8b0dea99ff57f24a4
f18charles <f.18charles@gmail.com>
2026-07-07 09:44:32 +0300

    style: add tailwind css packatge

--- frontend/piggy_bank/src/App.jsx
+++ frontend/piggy_bank/src/App.jsx
@@ -1,11 +1,12 @@
 import Layout from './components/Layout'
+import Dashboard from './pages/Dashboard'
 import './styles/App.css'
 
 function App() {
     return (
         <>
             <Layout>
-                
+                <Dashboard/>
             </Layout>
         </>
     )
