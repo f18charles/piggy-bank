2269d9f8e5f7eccba522e7303bb6154b5774074b
f18charles <f.18charles@gmail.com>
2026-07-07 12:12:16 +0300

    refactor(sidebar): use map to place list of items for the navigation sidebar
    To promote the DRY principle

--- frontend/piggy_bank/src/components/Layout/Sidebar.jsx
+++ frontend/piggy_bank/src/components/Layout/Sidebar.jsx
@@ -1,11 +1,17 @@
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
+    const navItems = [
+        {label: "Dashboard", path: "/"},
+    ]
+
     return (
         <>
-            <NavLink to="/" className={({isActive}) => isActive ? "bg-slate-700 text-white" : "text-slate-300" }>
-                Dashboard
-            </NavLink>
+            {navItems.map((item) => (
+                <NavLink key={item.path} to={item.path} className={({isActive}) => isActive ? "bg-slate-700 text-white" : "text-slate-300" }>
+                    {item.label}
+                </NavLink>
+            ))}       
         </>
     )
 }
