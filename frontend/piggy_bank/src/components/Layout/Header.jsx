2ca8dbf78a94988e7fe26361bf803de79205505f
f18charles <f.18charles@gmail.com>
2026-07-07 11:37:21 +0300

    refactor(header): create the header and add an image

--- frontend/piggy_bank/src/components/Layout/Header.jsx
+++ frontend/piggy_bank/src/components/Layout/Header.jsx
@@ -1,11 +1,17 @@
+import brand from "../../assets/piggybank.png"
+import { Link } from 'react-router-dom'
+
 const Header = () => {
     return (
         <>
-            <header class="w-full max-w-4xl mx-auto bg-white/90 backdrop-blur-sm rounded-2xl brand-header border-b border-gray-200/70 shadow-sm transition-all duration-200">
-                <nav class="px-6 py-4 md:px-8 md:py-5 flex items-center justify-start">
-                    <div class="text-2xl md:text-3xl font-semibold tracking-tight text-slate-800 hover:text-indigo-600 transition-colors duration-200 cursor-default">
-                        <span class="bg-gradient-to-r from-indigo-500 to-purple-600 bg-clip-text text-transparent">PiggyBank</span>
+            <header class="w-full bg-white/90 backdrop-blur-sm brand-header  border-gray-200/70 shadow-sm transition-all duration-200">
+                <nav class="px-0 py-2 md:px-0 md:py-2 flex items-center justify-start">
+                    <Link to="/">
+                    <div class="text-2xl md:text-3xl font-semibold tracking-tight text-slate-800 hover:text-indigo-600 transition-colors duration-200 cursor-default flex items-center">
+                        <img src={brand} className="w-30" alt="logo" />
+                        <span class="bg-gradient-to-r from-green-800 to-green-600 bg-clip-text text-transparent">PiggyBank</span>
                     </div>
+                    </Link>
                 </nav>
             </header>
         </>
