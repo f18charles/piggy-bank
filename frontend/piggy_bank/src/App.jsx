4901d11ce0f8079d1353812eafa05a5d35afa71d
f18charles <f.18charles@gmail.com>
2026-07-07 10:19:50 +0300

    feat(routes): add routes to main and app files for the routing

--- frontend/piggy_bank/src/App.jsx
+++ frontend/piggy_bank/src/App.jsx
@@ -1,14 +1,16 @@
 import Layout from './components/Layout/Layout'
+import { Routes, Route, Link } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import './styles/App.css'
 
 function App() {
     return (
-        <>
-            <Layout>
-                <Dashboard/>
-            </Layout>
-        </>
+        <Routes>
+            {/* <Route path="login" element={<Login />} /> */}
+            <Route path="/" element={<Layout />} >
+                <Route index element={<Dashboard />} />
+            </Route>
+        </Routes>
     )
 }
 
--- frontend/piggy_bank/src/main.jsx
+++ frontend/piggy_bank/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
