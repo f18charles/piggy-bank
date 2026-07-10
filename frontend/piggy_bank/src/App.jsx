b88242d9d8be658d1888bf64cc54e4ab001819eb
f18charles <f.18charles@gmail.com>
2026-07-10 12:54:02 +0300

    fix(App): correct login route path by adding leading slash

--- frontend/piggy_bank/src/App.jsx
+++ frontend/piggy_bank/src/App.jsx
@@ -8,7 +8,7 @@ import Login from './pages/auth/Login'
 function App() {
     return (
         <Routes>
-            <Route path="login" element={<Login />} />
+            <Route path="/login" element={<Login />} />
             <Route element={<ProtectedRoute />} >
                 <Route path="/" element={<Layout />} >
                     <Route index element={<Dashboard />} />
